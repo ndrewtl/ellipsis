5901a0ae2750d7b481158235c3328f6e1c0da20c
Andrew Lee <andrewl@mbda.fun>
2018-11-04 22:44:31 -0500

    Document git subcommand

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -7,8 +7,20 @@ use std::vec;
 // Import our result and home dir
 use ::{Error, ErrorKind, ext, Result, home_dir};
 
-// The function we're exporting
-// Print the ellipsis home directory
+///
+/// ellipsis git -> forward all given arguments to the git command
+///
+/// This function takes all given arguments in the variable `matches`, and forwards them to the
+/// `git(1)` command. This command is executed in the context of the ellipsis home directory.
+///
+/// # Errors
+///
+/// This function returns an error if the git command returns an error code. It also errors if the
+/// home directory does not exist or cannot be found.
+///
+/// # Panics
+/// This function panics if the ellipsis home directory cannot be located, that is if
+/// ['home_dir()'] returns `None`.
 pub fn run(matches : &clap::ArgMatches) -> Result<()> {
 
     let home = home_dir().expect("ellipsis home can't be found");
